927f2e5204b385e237779130d27f5edd4d2117af
ao <aolin@ymail.com>
2020-12-07 23:39:44 +0200

    added hooks to pages

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -1,1 +1,15 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css'
 
+barba.use(barbaCss);
+
+barba.init({
+    transitions: [
+        {
+            once() {
+
+            }
+        }
+    ]
+
+});
